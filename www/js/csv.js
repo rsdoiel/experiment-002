c5fb79916035123858f38cd197ae8db15f754580
R. S. Doiel <rsdoiel@gmail.com>
2013-10-05 19:58:36 -0700

    snapshot

--- www/js/csv.js
+++ www/js/csv.js
@@ -5,67 +5,84 @@
  * Released under the BSD 2-clause licensed.
  * See: http://opensource.org/licenses/BSD-2-Clause
  */
+/*jslint browser: true, indent: 4 */
+/*global console, modules */
 (function () {
     "use strict";
                 
     function parseLine(buf) {
         var delimiter = ',',
+            escape = '\\',
             quote = '',
             cut_from = 0,
-	    cut_length = 0,
+            cut_length = 0,
             cur = '',
             i = 0,
             l = 0,
             results = [];
+        
         buf = buf.trim();
-	console.log("DEBUG buf", buf);
+        console.log("DEBUG buf", buf);
+        cut_from = 0;
+        cut_length = 0;
         for (i = 0, l = buf.length; i < l; i += 1) {
             cur = buf[i];
-	    console.log("DEBUG cur", i, cur);
-            if (cut_from >= i) {
-                // We're looking for the start of a column
-		cut_length = 0;
-                if (cur === '"' || cur === "'") {
-                    quote = cur;
-                    cut_from = i + 1;
-                } else if (cur === delimiter) {
-                    results.push("");
-                    cut_from = -1;
-	            console.log("DEBUG pushing empty");
-		}
+            console.log([
+                "DEBUG i = [",
+                i,
+                "], cur = [",
+                cur,
+                "], cut_from = [",
+                cut_from,
+                "], cut_length = [",
+                cut_length,
+                "]"].join(""));
+            if (cur === delimiter && quote === '') {
+                // save the column's data
+                results.push(buf.substr(cut_from, cut_length).trim());
+                console.log("DEBUG push(" + cut_from +
+                            ", " + cut_length + ") -->" +
+                            results[results.length - 1]);
+                cut_from = i + 1;
+                cut_length = 0;
+                quote = '';
+            } else if (cur === escape && buf[i + 1] === quote) {
+                console.log("DEBUG escape " + cur + " -> " + buf[i + 1]);
+                i += 1;
+                cut_length += 1;
+            } else if ((cur === '"' || cur === "'") && quote === '') {
+                // Starting of a quote
+                quote = cur;
+                cut_from = i + 1;
+                cut_length = 0;
+            } else if (cur === quote) {
+                console.log("DEBUG end quote " + buf.substr(cut_from, cut_length));
+                // Ending of a quote
+                results.push(buf.substr(cut_from, cut_length).trim());
+                console.log("DEBUG push(" + cut_from +
+                            ", " + cut_length + ") -->" +
+                            results[results.length - 1]);
+                // Fast forward to next delimiter.
+                for (; i < l && buf[i] !== delimiter; i += 1) {
+                    // nothing to do but advance to the delimiter.
+                }
+                cut_from = i + 1;
+                cut_length = 0;
+                quote = '';
             } else {
-                // We in a column and looking for an end.
-                if (quote === '' && cur === delimiter) {
-                    // save the column's data
-                    results.push(buf.substr(cut_from, cut_length).trim());
-                    cut_from = i + 1;
-	            cut_length = 0;
-	            console.log("DEBUG pushing", results[results.length - 1]);
-                } else if (cur === '\\') {
-                    // We hit an escape char, skip ahead a character.
-                    i += 1;
-                } else if (cur === quote) {
-	            //clear the quote, set cut_length
-	            cut_length = i - cut_from;
-		    quote = '';
-                } else {
-		   // Update cut_length
-		   cut_length = (i - cut_from) + 1;
-		}
+                cut_length += 1;
             }
         }
-	console.log("DEBUG final i", i);
-	
-	if (buf.lastIndexOf(delimiter) === (buf.length - 1)) {
-	   results.push("");
-	   console.log("DEBUG push trailing empty colomn");
-	} else if (cut_length > 0) {
-	   results.push(buf.substr(cut_from).trim());
-	   console.log("DEBUG pushing unquoted trailing column");
-	}
+    
+        if (buf.trim().lastIndexOf(delimiter) === (buf.trim().length - 1)) {
+            results.push("");
+            console.log("DEBUG push trailing empty colomn");
+        } else if (cut_length > 0) {
+            results.push(buf.substr(cut_from).trim());
+            console.log("DEBUG pushing unquoted trailing column");
+        }
         return results;
     }
-                
     // Export as module
     modules("CSV", {parseLine: parseLine});
 }());
